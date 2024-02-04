0b94f8a6cdbc6cb2e47de3074f34fe0736294c68
Alice Gong <agong@coveo.com>
2024-02-04 03:07:07 -0500

    create query section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
+import { Search2Icon } from "@chakra-ui/icons";
+import { FaRegHospital } from "react-icons/fa6";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { LuFuel } from "react-icons/lu";
+
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
+import { FaFilter } from "react-icons/fa";
 
 const mapContainerStyle = {
   width: "100vw",
@@ -30,8 +48,113 @@ const Map = () => {
   );
 };
 
+const HeaderSection = () => (
+  <Stack bg="red" h="66px" maxW="100%" pointerEvents="auto" />
+);
+
+const QuerySection = () => (
+  <VStack h="11vh" gap="16px">
+    <InputGroup w="30%" pointerEvents="auto">
+      <InputLeftElement pointerEvents="none">
+        <Search2Icon color="gray.300" />
+      </InputLeftElement>
+      <Input placeholder="Enter an apartment address" variant="filled" />
+      <InputRightElement width="4.5rem">
+        <Button
+          h="1.75rem"
+          size="sm"
+          onClick={() => {
+            console.log("hihi");
+          }}
+        >
+          <FaFilter />
+
+          {/* {show ? "Hide" : "Show"} */}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+    <HStack>
+      <Button
+        leftIcon={<FaRegHospital />}
+        pointerEvents="auto"
+        colorScheme="teal"
+        variant="solid"
+        borderRadius="20px"
+      >
+        Hospital
+      </Button>
+      <Button
+        leftIcon={<MdOutlineLocalGroceryStore />}
+        colorScheme="teal"
+        variant="solid"
+        borderRadius="20px"
+      >
+        Grocery
+      </Button>
+      <Button
+        leftIcon={<LuFuel />}
+        colorScheme="teal"
+        variant="solid"
+        borderRadius="20px"
+      >
+        Gas station
+      </Button>
+      <Button
+        leftIcon={<FaRegHospital />}
+        pointerEvents="auto"
+        colorScheme="teal"
+        variant="solid"
+        borderRadius="20px"
+      >
+        Hospital
+      </Button>
+      <Button
+        leftIcon={<FaRegHospital />}
+        pointerEvents="auto"
+        colorScheme="teal"
+        variant="solid"
+        borderRadius="20px"
+      >
+        Hospital
+      </Button>
+    </HStack>
+  </VStack>
+);
+
+const ContentSection = () => (
+  <HStack
+    bg="pink"
+    h="calc(100vh - 67px - 11vh - 32px)"
+    maxW="100%"
+    justifyContent="space-between"
+  >
+    <Flex />
+    <Flex w="30%" height="100%" bg="grey" />
+  </HStack>
+);
+
 const IndexPage = () => {
-  return <Map />;
+  return (
+    <>
+      <Container maxW="100vw" bg="blue" centerContent>
+        <Map />
+        <Stack
+          pointerEvents="none"
+          //   bg="white"
+          flex="1"
+          w="100%"
+          pos="absolute"
+          top="0"
+          left="0"
+          gap="16px"
+        >
+          <HeaderSection />
+          <QuerySection />
+          <ContentSection />
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default IndexPage;
